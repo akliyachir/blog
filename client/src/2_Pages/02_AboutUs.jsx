d78f4de40fd018090e111d6dc137aff3e0ba5726
akliyachir <akli.yachir@gmail.com>
2023-06-06 03:12:13 -0300

    navigation

--- client/src/2_Pages/02_AboutUs.jsx
+++ client/src/2_Pages/02_AboutUs.jsx
@@ -3,10 +3,9 @@ import "./02_AboutUs.css";
 export default function AboutUs() {
   return (
     <div className="AboutUs">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quas
-      cupiditate ad sit corporis dolorem consectetur. Reiciendis blanditiis
-      architecto, optio voluptatem facilis tempora beatae ipsum, velit, natus
-      explicabo repellendus dolorum?
+      <h1>Qui Sommes-nous ?</h1>
+      <p>Nous, c'est moi-même ! Akli, votre serviteur</p>
+      <p>J'entamme</p>
     </div>
   );
 }
